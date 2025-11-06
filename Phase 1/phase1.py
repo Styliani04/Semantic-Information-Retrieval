b8b0e17f22374a8f065c494aaf78a5cb1f306139
guts <pokolo18@yahoo.com>
2025-11-06 20:06:14 +0200

    End of question b.
    What has been impemented so far:
    -Preprocessed the documents.csv file
    -Created a jsonl file with the preprocessesed texts, since that is the format that elastic search needs to create an index
    -Created the mapping with default analyzer and B25 similarity function
    -Created the index and filled it with each id/text json using bulk
    -Put the pretty response function
    
    #Note
    When the file is running it creates two files, one csv and onle jsonl. That is done for processing reasons. Also using the panda library.

--- Phase 1/phase1.py
+++ Phase 1/phase1.py
@@ -10,16 +10,15 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 import pandas as pd
 
+# initializing our elasticsearch port
 client = Elasticsearch("http://localhost:9200")
 
 # downloading the proper material for the preprocessing of the texts in documents.csv
-
 nltk.download("stopwords")
 nltk.download("punkt_tab")
 nltk.download('wordnet')
 
 # the preprocessing function
-
 def preprocess_english_text(text: str):
     # Normalize case
     text = text.lower()
@@ -50,7 +49,6 @@ file = os.path.join(file_directory, "documents.csv")
 df = pd.read_csv(file)
 
 # we want to process only the texts, not the ids
-
 texts = df["Text"]
 
 preprocessed_texts = []
@@ -61,7 +59,6 @@ for text in texts:
 )
 
 # we create a new csv file with the preprocessed texts
-
 preprocessed_text_df = pd.DataFrame({
     "ID" : df["ID"],
     "Text": preprocessed_texts
@@ -76,13 +73,19 @@ file2= os.path.abspath(file_path)
 
 df = pd.read_csv(file2)
 
-with open(file2, "w",  encoding="utf-8") as file:
+with open("preprocessed_documents.jsonl", "w",  encoding="utf-8") as file:
     for _, row in df.iterrows():
         json_data = {"ID": row["ID"], "Text": row["Text"]}
         file.write(json.dumps(json_data) + "\n")
 
-# creating our mapping with standard analyser and BM25 similarity
+# we take each json line from the jsonl file we created from the preprocessed_document.csv file and put it in a list
+json_data = [] 
+with open("preprocessed_documents.jsonl", "r", encoding="utf-8") as file:
+    for line in file:
+        data = json.loads(line)
+        json_data.append(data)
 
+# creating our mapping with standard analyser and BM25 similarity
 mapping = {
     "settings":{
         "analysis": {
@@ -125,3 +128,27 @@ if client.indices.exists(index="my_texts"):
 
 client.indices.create(index="my_texts", body=mapping)
 
+# adding the texts into the index with bulk since we have a big dataset
+documents = []
+for doc in json_data:
+    documents.append({
+        "_index": "my_texts",
+        "_id": doc["ID"],  
+        "_source": {
+            "Text": doc["Text"] 
+        }
+    })
+
+bulk(client, documents, refresh=True)
+
+# function for searching in a nice format //might not be needed
+def pretty_search_response(response):
+    if len(response["hits"]["hits"]) == 0:
+        print("Your search returned no results.")
+    else:
+        for hit in response["hits"]["hits"]:
+            id = hit["_id"]
+            text = hit["_text"]
+            
+            pretty_output = f"\nID: {id}\nText: {text}"
+            print(pretty_output)
