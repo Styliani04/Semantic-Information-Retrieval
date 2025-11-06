412e87a28dccf11a9ba21439696500c0120c0a7f
guts <pokolo18@yahoo.com>
2025-11-06 18:08:32 +0200

    Complete question a

--- Phase 1/phase1.py
+++ Phase 1/phase1.py
@@ -12,9 +12,13 @@ import pandas as pd
 
 client = Elasticsearch("http://localhost:9200")
 
+# downloading the proper material for the preprocessing of the texts in documents.csv
+
 nltk.download("stopwords")
 nltk.download("punkt_tab")
 nltk.download('wordnet')
+
+# the preprocessing function
 
 def preprocess_english_text(text: str):
     # Normalize case
@@ -39,19 +43,24 @@ def preprocess_english_text(text: str):
     
     return " ".join(preprocessed_words)
 
-
+# we find our documents.csv file in order to extract the texts
 file_directory = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join(file_directory, "documents.csv")
 
 df = pd.read_csv(file)
 
+# we want to process only the texts, not the ids
+
 texts = df["Text"]
 
 preprocessed_texts = []
 
+
 for text in texts:
     preprocessed_texts.append(preprocess_english_text(text)
 )
+
+# we create a new csv file with the preprocessed texts
 
 preprocessed_text_df = pd.DataFrame({
     "ID" : df["ID"],
@@ -59,3 +68,6 @@ preprocessed_text_df = pd.DataFrame({
 })
 
 preprocessed_text_df.to_csv("preprocessed_documents.csv",index=False)
+
+# we create a json file to use it in the indexing
+
