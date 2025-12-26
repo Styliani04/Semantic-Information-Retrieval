65d94f53f818b8a731253e3be7a698a431b10f0b
guts <pokolo18@yahoo.com>
2025-12-26 21:50:50 +0200

    Put the trec_eval. Havent run it yet since i couldnt find the elasicsearch file.

--- Phase2/phase2.py
+++ Phase2/phase2.py
@@ -4,8 +4,10 @@ import re as regex
 import numpy as np  
 from sentence_transformers import SentenceTransformer # pip install sentence-transformers, pip install tf-keras
 import faiss # pip install faiss-cpu
+import subprocess
+from elasticsearch import Elasticsearch
 
-
+client = Elasticsearch("http://localhost:9200")
 sys.stdout.reconfigure(encoding='utf-8')
 
 #1. Preprocessing the texts
@@ -67,3 +69,66 @@ for query in queries:
             distance = distances[0][i]
             similarity = 1 / (1 + distance)
             print(f"Rank {i+1:2}: Doc ID {id} | Similarity: {similarity} | Distance: {distance}")
+
+#5. Evaluating results using trec_eval    
+def run_queries_and_save_results(k, output_file):
+    queries = pd.read_csv("queries.csv")
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        for _, row in queries.iterrows():
+            query_id = row["ID"]
+            query_text = row["Text"]
+
+            response = client.search(
+                index="my_texts",
+                size=k,
+                track_total_hits=False,
+                query={
+                    "match": {
+                        "Text": query_text
+                    }
+                }
+            )
+            
+            # Sort hits by score in descending order
+            hits_sorted = sorted(response["hits"]["hits"], key=lambda x: x["_score"], reverse=True)
+
+            rank = 1
+            for hit in hits_sorted:
+                doc_id = hit["_id"]
+                score = hit["_score"]
+
+                f.write(f"{query_id} Q0 {doc_id} {rank} {score} myIRmethod\n")
+                rank += 1
+                
+for k in [20, 30, 50]:
+    output_file = f"results_{k}.txt"
+    run_queries_and_save_results(k, output_file)
+    print(f"Created {output_file}")
+
+qrels = "qrels.txt"
+run_files = {
+    "20": "results_20.txt",
+    "30": "results_30.txt", 
+    "50": "results_50.txt"
+}
+
+metrics = ["map", "P.5,10,15,20"] 
+
+for k, run_file in run_files.items():
+    output_file = f"eval_{k}.txt"
+    
+    cmd = ["trec_eval"]
+    for m in metrics:
+        cmd.extend(["-m", m])
+    cmd.extend([qrels, run_file])   # trec_eval -m map -m P.5,10,15,20 qrels.txt results_k.txt > eval_k.txt
+
+        
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True) # Running trec_eval command with silenced warnings
+        
+    if result.returncode == 0: # If trec_eval ran successfully
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(result.stdout)
+        print(f"Created {output_file}")
+    else:
+        print(f"Error running trec_eval for k={k}")
